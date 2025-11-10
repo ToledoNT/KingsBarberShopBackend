6ed90ba7557553e160565389d52892c4469bd7ff
ToledoNT <francisntoledo@hotmail.com>
2025-11-10 20:29:03 -0300

    DashBoard and Finance --fix

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,8 @@ import server from "./server";
 
 dotenv.config();
 
-// Middleware de log para depuração
-server.use((req, res, next) => {
-  console.log(`🚀 Request recebida: ${req.method} ${req.originalUrl}`);
-  console.log("Headers recebidos:", req.headers);
-  next();
-});
-
 const PORT = Number(process.env.PORT) || 4001;
 
-// escuta em todas as interfaces
 server.listen(PORT, "0.0.0.0", () => {
   console.clear();
   console.log(`🔥 Servidor rodando na porta ${PORT}`);
