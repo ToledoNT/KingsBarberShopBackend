55babe16603fb8a8bcf663175066310524fc6263
ToledoNT <francisntoledo@hotmail.com>
2025-12-02 10:49:11 -0300

    Prisma retry

--- src/db/prisma/prisma-connection.ts
+++ src/db/prisma/prisma-connection.ts
@@ -1,22 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
-export async function prismaConnect(retries = 10, delay = 5000) {
-  for (let i = 0; i < retries; i++) {
-    try {
-      await prisma.$connect();
-      console.log("✅ Conexão com o Prisma foi bem-sucedida!");
-      return;
-    } catch (error) {
-      console.error(`❌ Falha ao conectar com o Prisma. Tentativa ${i + 1} de ${retries}`);
-      console.error(error);
-      if (i < retries - 1) {
-        console.log(`⏳ Tentando novamente em ${delay / 1000}s...`);
-        await new Promise(res => setTimeout(res, delay));
-      }
-    }
+export async function prismaConnect() {
+  try {
+    await prisma.$connect();
+    console.log("Conexão com o Prisma foi bem-sucedida!");
+  } catch (error) {
+    console.error("Falha ao conectar com o Prisma:", error);
   }
-  console.error("🔥 Não foi possível conectar ao banco após várias tentativas.");
-  process.exit(1); // opcional: encerra a aplicação se não conectar
 }
